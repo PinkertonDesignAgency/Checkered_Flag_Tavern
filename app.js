739876e88e0411085aea26d38e9f5d88fb17da77
PinkertonDesignAgency <114441312+PinkertonDesignAgency@users.noreply.github.com>
2022-12-07 15:59:15 -0800

    changes but now shit is jacked

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-function onClickMenu(){
-    document.getElementById('menu').classList.toggle('change');
-}
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () =>{
+    navbarLinks.classList.toggle('active')
+})
